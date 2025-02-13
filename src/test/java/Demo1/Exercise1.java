76dac3212fa3feccacb992a6a07e1629707c0f88
nasrinna <nasrin.ahmad@noorteck.com>
2025-02-13 12:17:28 -0500

    add a new code from selenium

--- src/test/java/Demo1/Exercise1.java
+++ src/test/java/Demo1/Exercise1.java
@@ -1,5 +1,41 @@
 package Demo1;
 
-public class Exercise1 {
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.Hooks;
+
+public class Exercise1 extends Hooks{
+
+	public static void main(String[] args) throws InterruptedException {
+		setUp("safari");
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(3000);
+		
+		WebElement usernameField = driver.findElement(By.name("username"));
+		usernameField.sendKeys("admin" );
+		
+		Thread.sleep(3000);
+		
+		WebElement passwordField = driver.findElement(By.name("password"));
+		passwordField.sendKeys("admin123");
+		
+		Thread.sleep(3000);
+		
+		WebElement loginButton = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+		 
+		loginButton.click();
+		
+		Thread.sleep(3000);
+		
+	    WebElement HearderField = driver.findElement(By.className("oxd-topbar-header-breadcrumb")) 	;	
+	    HearderField.click();
+		 
+	     Thread.sleep(3000);
+		tearDown();
+		
+		
+	}
 }
---
+++ src/test/java/utils/Hooks.java
@@ -0,0 +1,42 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hooks {
+	public static WebDriver driver;
+
+	// method 1 --> opens browser
+	public static void setUp(String browserName) {
+		if (browserName.equals("edge")) {
+
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+
+		} else if (browserName.equals("safari")) {
+			WebDriverManager.safaridriver().setup();
+			driver = new SafariDriver();
+			
+		} else if (browserName.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		
+		//maximize the webpage
+		driver.manage().window().maximize();
+	}
+
+	// method2 --> closes the browser
+	public static void tearDown() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
+}
+
